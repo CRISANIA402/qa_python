6b474f0c61d7f400533c3cbe0a7a0b030b085e37
Dasha Kolyadina <CrisaniaPaladaina@yandex.ru>
2026-08-16 03:09:21 +0300

    коммит1

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +23,83 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize('name', [
+        'А' * 40,
+        'Книга',
+    ])
+    def test_add_new_book_valid_names(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name in collector.get_books_genre()
+
+    @pytest.mark.parametrize('name', [
+        'А' * 41,
+    ])
+    def test_add_new_book_invalid_names(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name not in collector.get_books_genre()
+
+    def test_add_new_book_no_duplicates(self):
+        collector = BooksCollector()
+        collector.add_new_book("Властелин колец")
+        collector.add_new_book("Властелин колец")
+        assert len(collector.books_genre) == 1
+
+    def test_set_book_genre_success(self):
+        collector = BooksCollector()
+        collector.add_new_book("Мизери")
+        collector.set_book_genre("Мизери", "Ужасы")
+        assert collector.get_book_genre("Мизери") == "Ужасы"
+
+    def test_set_book_genre_invalid_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга без жанра")
+        collector.set_book_genre("Книга без жанра", "Некорректный жанр")
+        assert collector.get_book_genre("Книга без жанра") == ""
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.add_new_book('Сияние')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Оно', 'Ужасы')
+        collector.set_book_genre('Сияние', 'Ужасы')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Ужасы') == ['Оно', 'Сияние']
+
+    def test_get_books_for_children(self):
+       collector = BooksCollector()
+       collector.add_new_book('Мизери')
+       collector.add_new_book('Шерлок Холмс')
+       collector.add_new_book('Звездные войны')
+       collector.set_book_genre('Мизери', 'Ужасы')
+       collector.set_book_genre('Шерлок Холмс', 'Детективы')
+       collector.set_book_genre('Звездные войны', 'Фантастика')
+       assert collector.get_books_for_children() == ['Звездные войны']
+
+    def test_add_book_in_favorites_success(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга избранное")
+        collector.add_book_in_favorites("Книга избранное")
+        assert "Книга избранное" in collector.get_list_of_favorites_books()
+
+    def test_add_book_in_favorites_book_not_in_collection(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites("Книга без коллекции")
+        assert "Книга без коллекции" not in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга 1")
+        collector.add_new_book("Книга 2")
+        collector.add_book_in_favorites("Книга 1")
+        collector.add_book_in_favorites("Книга 2")
+
+        collector.delete_book_from_favorites("Книга 1")
+
+        favorites = collector.get_list_of_favorites_books()
+        assert "Книга 1" not in favorites
+        assert "Книга 2" in favorites
+    
